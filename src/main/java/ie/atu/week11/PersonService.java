0ed87306df2a71420ce002ab895c02cbacc40bb7
Antoj2000 <g00385306@atu.ie>
2024-11-26 10:28:25 +0000

    Project created

---
+++ src/main/java/ie/atu/week11/PersonService.java
@@ -0,0 +1,17 @@
+package ie.atu.week11;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+    // Placeholder method to save a person (add to Db in the next two weeks)
+    public void savePerson(Person person) {
+        System.out.println("Person saved: " + person);
+    }
+
+    // Placeholder method to retrieve a person by employeeId
+    public Person getPersonByEmployeeId(String employeeId) {
+        // fetch data from a database in future lab
+        // For simplicity, we return a dummy person here
+        return new Person();
+    }
+}
